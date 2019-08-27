d6a0ed7d0f753eac08af32bbfda438eb496e3433
Fiskee <fisker@gmail.com>
2019-08-27 13:54:56 +0200

    Add Shoot

--- Enums/Spells/Global.lua
+++ Enums/Spells/Global.lua
@@ -3,7 +3,8 @@ local DMW = DMW
 DMW.Enums.Spells = {
     GLOBAL = {
         Abilities = {
-            Attack = {Ranks = {6603}}
+            Attack = {Ranks = {6603}},
+            Shoot = {Ranks = {5019}}
         },
         Buffs = {},
         Debuffs = {},
