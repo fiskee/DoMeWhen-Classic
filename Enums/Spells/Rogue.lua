86d4088073f02300323aff72f777bc8d3d2444d3
Fiskee <fisker@gmail.com>
2019-08-28 02:58:29 +0200

    Rogue spells

--- Enums/Spells/Rogue.lua
+++ Enums/Spells/Rogue.lua
@@ -1,8 +1,46 @@
 local Spells = DMW.Enums.Spells
 
 Spells.ROGUE = {
-    Abilities = {},
-    Buffs = {},
+    Abilities = {
+        Ambush = {Ranks = {8676, 8724, 8725, 11267, 11268, 11269}},
+        Backstab = {Ranks = {53, 2589, 2590, 2591, 8721, 11279, 11280, 11281, 25300}},
+        Blind = {Ranks = {2094}},
+        CheapShot = {Ranks = {1833}},
+        DetectTraps = {Ranks = {2836}},
+        DisarmTraps = {Ranks = {1842}},
+        Distract = {Ranks = {1725}},
+        Evasion = {Ranks = {5277}},
+        Eviscerate = {Ranks = {2098, 6760, 6761, 6762, 8623, 8624, 11129, 11300, 31016}},
+        ExposeArmor = {Ranks = {8647, 8649, 8650, 11197, 11198}},
+        Feint = {Ranks = {1966, 6768, 8637, 11303, 25302}},
+        Garrote = {Ranks = {703, 8631, 8632, 8633, 11289, 11290}},
+        Gouge = {Ranks = {1776, 1777, 8629, 11285, 11286}},
+        Hemorrhage = {Ranks = {17347, 17348}},
+        Kick = {Ranks = {1766, 1767, 1768, 1769}},
+        KidneyShot = {Ranks = {408, 8643}},
+        PickLock = {Ranks = {1804}},
+        PickPocket = {Ranks = {921}},
+        Poisons = {Ranks = {2842}},
+        Rupture = {Ranks = {1943, 8639, 8640, 11273, 11274, 11275}},
+        SafeFall = {Ranks = {1860}},
+        Sap = {Ranks = {6770, 2070, 11297}},
+        SinisterStrike = {Ranks = {1752, 1757, 1758, 1759, 1760, 8621, 11293, 11294}},
+        SliceAndDice = {Ranks = {5171, 6774}},
+        Sprint = {Ranks = {2983, 8696, 11305}},
+        Stealth = {Ranks = {1784, 1785, 1786, 1787}},
+        Vanish = {Ranks = {1856}},
+        BlindingPowder = {Ranks = {6510}},
+        CripplingPoison = {Ranks = {3420}},
+        CripplingPoison2 = {Ranks = {3421}},
+        DeadlyPoison = {Ranks = {2835, 2837, 11357, 11358, 25347}},
+        InstantPoison = {Ranks = {8681, 8687, 8691, 11341, 11342, 11343}},
+        MindNumbingPoison = {Ranks = {5763, 8694, 11400}},
+        WoundPoison = {Ranks = {13220, 13228, 13229, 13230}}
+    },
+    Buffs = {
+        Remorseless = {Ranks = {14143, 14149}},
+        SliceAndDice = {Ranks = {5171, 6774}}
+    },
     Debuffs = {},
     Talents = {}
 }
