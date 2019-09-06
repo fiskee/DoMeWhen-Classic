5346484f5d8b0a92c2a29377dcb8f8f7915c3eb5
Fiskee <fisker@gmail.com>
2019-09-06 03:11:31 +0200

    Druid dispels

--- Enums/Dispel.lua
+++ Enums/Dispel.lua
@@ -1,4 +1,8 @@
 DMW.Enums.DispelSpells = {
+    --Druid
+    [2893] = {"Poison"}, -- Abolish Poison
+    [8946] = {"Poison"}, -- Cure Poison
+    [2782] = {"Curse"}, -- Remove Curse
     --Priest
     [527] = {"Magic"},
     [988] = {"Magic"},
@@ -6,10 +10,6 @@ DMW.Enums.DispelSpells = {
     [370] = {"Magic"}
 }
 
-DMW.Enums.SpecialDispel = {
+DMW.Enums.SpecialDispel = {}
 
-}
-
-DMW.Enums.NoDispel = {
-
-}
+DMW.Enums.NoDispel = {}
