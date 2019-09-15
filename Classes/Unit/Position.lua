a6a0b7d4e0a46ad9a6a2e62c49036bb905bb65aa
Fiskee <fisker@gmail.com>
2019-09-15 08:34:43 +0200

    Prepare facing for new EWT version, maybe breaks

--- Classes/Unit/Position.lua
+++ Classes/Unit/Position.lua
@@ -2,7 +2,7 @@ local DMW = DMW
 local Unit = DMW.Classes.Unit
 
 function Unit:RawFacing()
-    return ReadMemory(tonumber(self.Pointer) + 0x19E8, "float")
+    return select(2, ObjectFacing(self.Pointer))
 end
 
 function Unit:GetFacingAngle(OtherUnit)
@@ -10,6 +10,9 @@ function Unit:GetFacingAngle(OtherUnit)
     local X = OtherUnit.PosX - self.PosX
     local Y = OtherUnit.PosY - self.PosY
     local Angle = self:RawFacing()
+    if Angle > math.pi then
+        Angle = Angle - (2 * math.pi)
+    end
     local DX = math.cos(Angle)
     local DY = math.sin(Angle)
     return(math.abs(math.atan2(X * DY - Y * DX,  X * DX + Y * DY ) * (180 / math.pi)))
