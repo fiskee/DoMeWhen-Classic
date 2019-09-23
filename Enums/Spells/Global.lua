3355b3d431398b0ba83ce1cf0b4b4f671ba6afb8
hz <akahz@bk.ru>
2019-09-23 08:54:51 +0200

    orc racial

--- Enums/Spells/Global.lua
+++ Enums/Spells/Global.lua
@@ -4,6 +4,7 @@ DMW.Enums.Spells = {
     GLOBAL = {
         Abilities = {
             Attack = {Ranks = {6603}},
+            BloodFury = {Ranks = {20572}},
             Shoot = {Ranks = {5019}}
         },
         Buffs = {},
