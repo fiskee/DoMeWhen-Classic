f01152748dfb368f042b49ae75f4fdfff2325a29
Hassliebe <thorben@hassliebe.net>
2019-11-19 15:10:27 +0100

    Add files via upload

--- Enums/Spells/Rogue.lua
+++ Enums/Spells/Rogue.lua
@@ -2,10 +2,13 @@ local Spells = DMW.Enums.Spells
 
 Spells.ROGUE = {
     Abilities = {
+        AdrenalineRush = {Ranks = {13750}},
         Ambush = {Ranks = {8676, 8724, 8725, 11267, 11268, 11269}},
         Backstab = {Ranks = {53, 2589, 2590, 2591, 8721, 11279, 11280, 11281, 25300}},
+        BladeFlurry = {Ranks = {13877}},
         Blind = {Ranks = {2094}},
         CheapShot = {Ranks = {1833}},
+		ColdBlood = {Ranks = {14177}},
         DetectTraps = {Ranks = {2836}},
         DisarmTraps = {Ranks = {1842}},
         Distract = {Ranks = {1725}},
@@ -21,6 +24,7 @@ Spells.ROGUE = {
         PickLock = {Ranks = {1804}},
         PickPocket = {Ranks = {921}},
         Poisons = {Ranks = {2842}},
+        Riposte = {Ranks = {14251}},
         Rupture = {Ranks = {1943, 8639, 8640, 11273, 11274, 11275}},
         SafeFall = {Ranks = {1860}},
         Sap = {Ranks = {6770, 2070, 11297}},
@@ -38,10 +42,74 @@ Spells.ROGUE = {
         WoundPoison = {Ranks = {13220, 13228, 13229, 13230}}
     },
     Buffs = {
+        AdrenalineRush = {Ranks = {13750}},
+		ColdBlood = {Ranks = {14177}},
         Remorseless = {Ranks = {14143, 14149}},
         SliceAndDice = {Ranks = {5171, 6774}},
-        Stealth = {Ranks = {1784, 1785, 1786, 1787}}
+        Stealth = {Ranks = {1784, 1785, 1786, 1787}},
+        Sprint = {Ranks = {2983, 8696, 11305}},
+        InstantPoison = {Ranks = {323, 8681, 8687, 8691, 11341, 11342, 11343}}
     },
-    Debuffs = {},
-    Talents = {}
+    Debuffs = {
+        Blind = {Ranks = {2094}},
+        CheapShot = {Ranks = {1833}},
+        Gouge = {Ranks = {1776, 1777, 8629, 11285, 11286}},
+        KidneyShot = {Ranks = {408, 8643}},
+        Sap = {Ranks = {6770, 2070, 11297}}
+    },
+    Talents = {
+        -- Assassination
+        ImprovedEviscerate = {1, 1},
+        RemorselessAttacks = {1, 2},
+        Malice = {1, 3},
+        Ruthlessness = {1, 4},
+        Murder = {1, 5},
+        ImprovedSliceAndDice = {1, 6},
+        RelentlessStrikes = {1, 7},
+        ImprovedExposeArmor = {1, 8},
+        Lethality = {1, 9},
+        VilePoisons = {1, 10},
+        ImprovedPoisons = {1, 11},
+        ColdBlood = {1, 12},
+        ImprovedKidneyShot = {1, 13},
+        SealFate = {1, 14},
+        Vigor = {1, 15},
+        -- Combat
+        ImprovedGouge = {2, 1},
+        ImprovedSinisterStrike = {2, 2},
+        LightningReflexes = {2, 3},
+        ImprovedBackstab = {2, 4},
+        Deflection = {2, 5},
+        Precision = {2, 6},
+        Endurance = {2, 7},
+        Riposte = {2, 8},
+        ImprovedSprint = {2, 9},
+        ImprovedKick = {2, 10},
+        DaggerSpecialization = {2, 11},
+        DualWieldSpecialization = {2, 12},
+        MaceSpecialization = {2, 13},
+        BladeFlurry = {2, 14},
+        SwordSpecialization = {2, 15},
+        FistWeaponSpecialization = {2, 16},
+        WeaponExpertise = {2, 17},
+        Aggression = {2, 18},
+        AdrenalineRush = {2, 19},
+        -- Subtlety
+        MasterOfDeception = {3, 1},
+        Opportunity = {3, 2},
+        SleightOfHand = {3, 3},
+        Elusiveness = {3, 4},
+        Initiative = {3, 5},
+        GhostlyStrike = {3, 6},
+        ImprovedAmbush = {3, 7},
+        Setup = {3, 8},
+        ImprovedSap = {3, 9},
+        SerratedBlades = {3, 10},
+        HeightenedSenses = {3, 11},
+        Preparation = {3, 12},
+        DirtyDeeds = {3, 13},
+        Hemorrhage = {3, 14},
+        Deadliness = {3, 15},
+        Premeditation = {3, 16}
+    }
 }
