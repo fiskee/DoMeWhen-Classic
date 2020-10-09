b93006c427be4b9fa72a3b65c47bdedb524f8d0a
Stefan <schadis@gmx.at>
2020-10-09 09:08:20 +0200

    removed douple Entries and rearrange

--- Enums/Items.lua
+++ Enums/Items.lua
@@ -4,6 +4,7 @@ DMW.Enums.Items = {
     ManaCitrine = 8007,
     ManaJade = 5513,
     ManaRuby = 8008,
+
     -- Mage Food & Drink
     ConjuredWater = 5350,
     ConjuredFreshWater = 2288,
@@ -27,6 +28,7 @@ DMW.Enums.Items = {
     Healthstone = 5509,
     GreaterHealthstone = 5510,
     MajorHealthstone = 9421,
+
     -- Potions: Health
     MinorHealingPotion = 118,
     LesserHealingPotion = 858,
@@ -36,6 +38,8 @@ DMW.Enums.Items = {
     SuperiorHealingPotion = 3928,
     CombatHealingPotion = 18839,
     MajorHealingPotion = 13446,
+    LimitedInvulnerabilityPotion = 3387,
+
     -- Potions: Mana
     MinorManaPotion = 2455,
     LesserManaPotion = 3385,
@@ -44,16 +48,22 @@ DMW.Enums.Items = {
     SuperiorManaPotion = 13443,
     CombatManaPotion = 18841,
     MajorManaPotion = 13444,
+
     -- Potions: Rejuvenation
     MinorRejuvenationPotion = 2456,
     RejuvenationPotion = 22729,
     MajorRejuvenationPotion = 18253,
-    --Soulstones
+
+    --Potions: Combat
+    LimitedInvulnerabilityPotion = 3387,
+
+    -- Soulstones
     MinorSoulstone = 5232,
     LesserSoulstone = 16892,
     Soulstone = 16893,
     GreaterSoulstone = 16895,
     MajorSoulstone = 16896,
+
     --Trinkets
     Luffa = 19141,
     DevilsaurEye = 19991,
@@ -64,14 +74,6 @@ DMW.Enums.Items = {
     RoyalSeal = 18473,
     BlackhandsB = 13965,
 
-    --Potions
-    SuperiorHealingPotion = 3928,
-    GreaterHealingPotion = 1710,
-    HealingPotion = 929,
-    LesserHealingPotion = 858,
-    MinorHealingPotion = 118,
-    LimitedInvulnerabilityPotion = 3387,
-	
     --Engineering
     GoblinSapperCharge = 10646,
     DenseDynamite = 18641,		-- 340-460 range 30y
